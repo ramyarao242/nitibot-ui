4a54789a21dfc7ab7e286a9254be27f565543e3d
ramyarao242 <ramyarao242@gmail.com>
2025-05-18 10:34:38 -0400

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { useEffect } from 'react'
+
+function App() {
+  useEffect(()=>{
+    // if(window.tailwind){
+    //   window.tailwind.config={
+
+    //   }
+    // }
+  },[])
+  return(
+    <div className='bg-yellow-300 text-center p-6 text-3x1 font-bold'>
+      tailwind is working !
+    </div>
+  )
+}
+
+export default App
