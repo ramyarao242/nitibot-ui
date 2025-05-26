83cee78785223b46e1af0eadf747a493219e66fe
ramyaashokrao <ramyaashokrao@gmail.com>
2025-05-26 11:48:48 +0000

    Updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ export default {
                 'wisdom-level-1-text': '#333333', // Dark gray text color
                 'wisdom-level-1-button': '#FAE2AB', // Button color
                 'nitibot-title': '#815317', // Light brown/gold color for the title
+                'nitibot-title-level-2': '#906b35',
+                'level-2-button': '#e7cb80',
+                'wisdom-level-2': '#f9eed1',
                 'wisdom-level-1-button-text': '#2F0800', // Button text color
             },
         }
