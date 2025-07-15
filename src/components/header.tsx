51f7e09f35770d7b8227f1b5585b60c4d9a108d4
ramyarao242 <ramyarao242@gmail.com>
2025-07-15 04:33:14 -0400

    fix

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -41,19 +41,19 @@ const Header: React.FC = () => {
       </Flex>
 
       <div className="header-buttons" style={{ display: 'flex', gap: '10px', marginLeft: '40px', justifyContent: 'center' }}  >
-      <Button className="transparent-btn" onClick={() => navigate("/")}><span class="material-symbols-outlined">
+      <Button className="transparent-btn" onClick={() => navigate("/")}><span className="material-symbols-outlined">
 home
 </span>Home</Button>
-        <Button className="transparent-btn" onClick={() => navigate("/ask")}> <span class="material-symbols-outlined">
+        <Button className="transparent-btn" onClick={() => navigate("/ask")}> <span className="material-symbols-outlined">
 forum
 </span>Ask Chanakya</Button>
-        <Button className="transparent-btn" onClick={() => navigate("/library")}><span class="material-symbols-outlined">
+        <Button className="transparent-btn" onClick={() => navigate("/library")}><span className="material-symbols-outlined">
 book_2
 </span> Explore Library</Button>
-        <Button className="transparent-btn" onClick={() => navigate("/quiz")}><span class="material-symbols-outlined">
+        <Button className="transparent-btn" onClick={() => navigate("/quiz")}><span className="material-symbols-outlined">
 quiz
 </span>Quiz</Button>
-        <Button className="transparent-btn" onClick={() => navigate("/chronicles")}><span class="material-symbols-outlined">
+        <Button className="transparent-btn" onClick={() => navigate("/chronicles")}><span className="material-symbols-outlined">
 auto_stories
 </span>Chanakya Chronicles</Button>
       </div>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,33 +5,37 @@ import { Box, Button, Heading, Input, Text, SimpleGrid } from '@chakra-ui/react'
 const features = [
   {
     title: "Chanakya Neeti for the day",
-    description: "Take on a new challenge every day to sharpen your mind.",
-    route: "/challenge",
+    description: "",
+    actionBtn: false,
   },
   {
     title: "Daily Challenge",
     description: "Take on a new challenge every day to sharpen your mind.",
-    route: "/challenge",
+    actionBtn: false,
   },
   {
     title: "Ask Chanakya",
     description: "Ask any question and get wisdom from Chanakya.",
     route: "/ask",
+    actionBtn: false,
   },
   {
     title: "Chanakya Verses Library",
     description: "Read all verses and lessons from Chanakya Neeti.",
     route: "/library",
+    actionBtn: true,
   },
   {
     title: "Quiz",
     description: "Test your knowledge with this fast paced quiz.",
     route: "/quiz",
+    actionBtn: true,
   },
   {
     title: "Chanakya Chronicles",
     description: "Explore the life and teachings of Chanakya.",
     route: "/chronicles",
+    actionBtn: true,
   },
 ];
 
@@ -70,6 +74,14 @@ const Home: React.FC = () => {
             <Text color="subtitle" mb={4} textAlign="center">
               {feature.description}
             </Text>
+            {feature.actionBtn && (
+              <Button
+                className='action-btn'
+                onClick={() => navigate(feature.route)}
+              >
+                Go
+              </Button>
+            )}
             {feature.title === "Ask Chanakya" ? (
               <Box w="full" display="flex" flexDirection="column" alignItems="center"  bg="background">
                 <Input
@@ -87,13 +99,13 @@ const Home: React.FC = () => {
                   Ask
                 </Button>
               </Box>
-            ) : (
-              <Button
-                className='action-btn'
-                onClick={() => navigate(feature.route)}
-              >
-                Go
-              </Button>
+            ) : null}
+            {feature.title === "Chanakya Neeti for the day" && (
+              <Box w="full" display="flex" flexDirection="column" alignItems="center"  >
+                <Text color="subtitle" textAlign="center">
+                  "The world's biggest power is the youth and beauty of a woman."
+                </Text>
+              </Box>
             )}
           </Text>
         ))}
